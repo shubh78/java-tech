04588c442f9f51a7eeab276d48ec8090ace51b60
shubhamy1997 <shubhamyaduwanshi1997@gmail.com>
2021-01-23 12:34:26 +0530

    maven commit

---
+++ src/main/java/com/javatech/Java/Tech/JavaTechApplication.java
@@ -0,0 +1,13 @@
+package com.javatech.Java.Tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatech/Java/Tech/JavaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatech.Java.Tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
